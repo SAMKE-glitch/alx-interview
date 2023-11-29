130d4bda1dc0ca0934ab122a73ac816ed8d1c3e9
SAMKE-glitch <sammythemwa@gmail.com>
2023-11-29 16:35:01 +0300

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    A method that returns a list of integers
+    representing the Pascal's triangle of n
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle

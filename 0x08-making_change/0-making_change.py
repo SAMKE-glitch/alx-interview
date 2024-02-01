baa31218540de0589f04dfbd1e45cdaf0e93a2a8
SAMKE-glitch <sammythemwa@gmail.com>
2024-02-01 17:45:11 +0300

    optimized the code

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,16 +14,13 @@ def makeChange(coins, total):
 
     # Sort coins in descending order
     coins.sort(reverse=True)
-
-    # Create a list to store the fewest number of coins
-    # needed for each total from 0 to the target total
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
+    noOfCoins = 0
     for coin in coins:
-        for i in range(coin, total + 1):
-            if i - coin >= 0:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-            else:
-                break
-    return dp[total] if dp[total] != float('inf') else -1
+        if total <= 0:
+            break
+        temp = total // coin
+        noOfCoins += temp
+        total -= (temp * coin)
+    if total != 0:
+        return -1
+    return noOfCoins

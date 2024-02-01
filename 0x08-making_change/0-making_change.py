9df09ecaaa0c85ddb2fdf22c04e4cee732c908b1
SAMKE-glitch <sammythemwa@gmail.com>
2024-02-01 17:35:57 +0300

    made changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,6 +11,10 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
+
+    # Sort coins in descending order
+    coins.sort(reverse=True)
+
     # Create a list to store the fewest number of coins
     # needed for each total from 0 to the target total
     dp = [float('inf')] * (total + 1)
@@ -18,7 +22,8 @@ def makeChange(coins, total):
 
     for coin in coins:
         for i in range(coin, total + 1):
-            # update dp[i] if using current coin reduces
-            # the number of coins needed
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+            if i - coin >= 0:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+            else:
+                break
     return dp[total] if dp[total] != float('inf') else -1
